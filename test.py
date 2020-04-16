46ca37958cd29fc913b3d922181c5e424fa7e1d0
arjenbin <info@abbesturingstechniek.nl>
2020-04-16 22:23:09 +0200

    new features

--- test.py
+++ test.py
@@ -1,34 +1,57 @@
+import os
+import time
+
 #Make array with some standard cars in it
 arrayCars = ['merc', 'volvo', 'bmw']
 
 #Get car from array
 def getCar(LineRequest):
+    cls()
     CurrentCar = arrayCars[LineRequest]
     print(CurrentCar)
     EnterCommand()
     
 #Write car to array
 def setCar(LineRequest, NewCar):
+    cls()
     arrayCars[LineRequest] = NewCar
     EnterCommand()
     
 #Check if user input was correct    
 def CheckCommand(Command,Check):
-    return Command.lower() == Check.lower():
+    return Command.lower() == Check.lower()
 
 #Check if user input was correct    
 def CheckLine(Command,Check):
-    return int(Command) < Check:
- 
+    return int(Command) < Check
+
+#Print total Array
+def ShowList():
+    cls()
+    for (index,item) in enumerate (arrayCars, start = 0):
+        print(index,':',item)
+    EnterCommand()
+
 #Try again message
 def TryAgain():
     print("Try again")
+    time.sleep(0.5)
+    cls()
     EnterCommand() 
-    
+
+def cls():
+    os.system('cls')
 #Get user command    
-def EnterCommand():  
-    UserCommand = input('Request Car = R, Edit Car = E:')
+def EnterCommand():
+    #-----Main screen-----#
+    UserCommand = input(
+    '\n'
+    '-----------------\n'
+    'Request Car = R\n'
+    'Edit Car = E\n'
+    'Show List = L\n')
     
+    #-----Wait for user command-----#
     if CheckCommand(UserCommand,'r'):
         LineRequest = input('enter line request:')
         if CheckLine(LineRequest, len(arrayCars)):
@@ -42,10 +65,11 @@ def EnterCommand():
             setCar(int(LineRequest),NewCar)
         else:
             TryAgain()
+    if CheckCommand(UserCommand,'l'):
+            ShowList()
     else:
         TryAgain()
         
 #Main    
 EnterCommand()
 
-changes here
